4de6b9adc18894097d40952bf156696df5b42fdf
Heramb.0x1 <136613738+Heramb0x1@users.noreply.github.com>
2024-08-03 13:38:23 +0530

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,34 @@
+const currentPage = window.location.pathname;
+const pageNames = {
+  '/': 'home',
+  '/posts/post.html': 'posts',
+  '/about/about.html': 'about',
+  '/posts/blog1.html': 'welcome',
+};
+
+function generateBreadcrumbs() {
+  const breadcrumbContainer = document.querySelector('.breadcrumbs');
+  const pathSegments = currentPage.split('/').filter(segment => segment !== '');
+
+  let breadcrumbPath = '<a href="/">home</a>';
+
+  if (currentPage === '/posts/post.html') {
+    breadcrumbPath += ` / <a href="/posts/post.html">posts</a>`;
+  } else {
+    for (let i = 1; i < pathSegments.length; i++) {
+      const path = `/${pathSegments.slice(0, i + 1).join('/')}`;
+      let pageName = pageNames[path] || pathSegments[i];
+
+      if (path === '/posts/blog1.html') {
+        breadcrumbPath +=
+            ` / <a href="/posts/post.html">posts</a> / ${pageName}`;
+      } else {
+        breadcrumbPath += ` / <a href="${path}">${pageName}</a>`;
+      }
+    }
+  }
+
+  breadcrumbContainer.innerHTML = breadcrumbPath;
+}
+
+generateBreadcrumbs();

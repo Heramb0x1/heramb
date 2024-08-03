40b9d82a4fe7550e0ddf15a6332d021bab6841c3
Heramb.0x1 <136613738+Heramb0x1@users.noreply.github.com>
2024-08-04 03:19:25 +0530

    Update mains.js

--- mains.js
+++ mains.js
@@ -1,12 +1,12 @@
 const currentPage = window.location.pathname;
 const pageNames = {
   '/': 'home',
-  '/posts/post.html': 'posts',
+    '/posts/post.html': 'posts',
   '/about/about.html': 'about',
   '/projects/projects.html': 'projects',
+  '/projects/project1.html': 'MoodE : Mood Based Environment Development',
   '/posts/blog0.html': 'welcome',
   '/posts/blog1.html': 'Web 3.0, AI, and the Future of the Internet',
-  '/projects/project1.html': 'MoodE : Mood Based Environment Development',
 };
 
 function generateBreadcrumbs() {
@@ -29,12 +29,6 @@ function generateBreadcrumbs() {
         breadcrumbPath += ` / <a href="${path}">${pageName}</a>`;
       }
 
-      if (path === '/projects/project1.html') {
-        breadcrumbPath +=
-            ` / <a href="/projects/project.html">projects</a> / ${pageName}`;
-      } else {
-        breadcrumbPath += ` / <a href="${path}">${pageName}</a>`;
-      }
     }
   }
 
